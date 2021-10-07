ef41135f1d850ec2936e19b655c2738ea5cbe6de
Sabarikandhasami <kandhasamisabari@gmail.com>
2021-10-07 12:55:55 +0530

    hi first attempt

---
+++ src/test/java/com/two/Da.java
@@ -0,0 +1,16 @@
+package com.two;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Da {
+public static void main(String[] args) {
+	Date date=new Date();
+	System.out.println(date);
+	SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MM-yy");
+	String format = dateFormat.format(date);
+	System.out.println(format);
+}
+	
+
+}
